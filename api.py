b9cb3134696f3a4a6effc9d5907eb5cc140dc942
William Cottrell <williamcottrell72@gmail.com>
2018-09-05 10:18:42 -0700

    changed a bunch of stuff

--- api.py
+++ api.py
@@ -3,79 +3,33 @@ import numpy as np
 import pickle
 #import pandas
 
-pipeline=pickle.load(open('./model/gscv_rf.pkl','rb'))
-# pipeline=pickle.load(open('./model/dummy.pkl','rb'))
-# df_city=pickle.load(open('./model/city_density.pkl','rb'))
-pop_dct=pickle.load(open('./model/pop_dct','rb'))
-population_dct=pickle.load(open('./model/population_dct','rb'))
-dct_density=pickle.load(open('./model/density_dct','rb'))
-
-# dct_density={df_city.iloc[i,0]:df_city.iloc[i,1] for i in range(len(df_city))}
-#dct_pop={df_city.iloc[i,0]:df_city.iloc[i,1] for i in range(len(df_city))}
-
-
-# features['population'], features['density']
-
-example = {
-  'goal': 3,  # int
-  'duration': 4,    # M or F
-  'month': 22,    # int
-  'level_num': 1,  # int
-  'popularity': 0,  # int
-  'density': 7.25,
-   'population': 10# float
-}
 
 
 
 
-
-def month_conv(x):
-    if x=='Jan':
-        return 1
-    elif x=='Feb':
-        return 2
-    elif x=='Mar':
-        return 3
-    elif x=='Apr':
-        return 4
-    elif x=='May':
-        return 5
-    elif x=='June':
-        return 6
-    elif x=='July':
-        return 7
-    elif x=='Aug':
-        return 8
-    elif x=='Sep':
-        return 9
-    elif x=='Oct':
-        return 10
-    elif x=='Nov':
-        return 11
-    elif x=='Dec':
-        return 12
 
 
 def make_prediction(features):
 
     #print(features['city'])
-    cit=features['city']
-    popularity=pop_dct[features['category']]
+    # cit=features['city']
+    # popularity=pop_dct[features['category']]
 
-    mnth=month_conv(features['month2'])
+    # mnth=month_conv(features['month2'])
+    #
+    #
+    # population=population_dct[cit]
+    #
+    # density=dct_density[cit]
+
+    # print(f"The density is {density}")
+
+    # X = np.array([features['goal2'], features['duration2'],mnth,features['num_levels'],popularity,density,population]).reshape(1,-1)
+    # prob = pipeline.predict_proba(X)[0, 1]
 
 
-    population=population_dct[cit]
 
-    density=dct_density[cit]
-
-    print(f"The density is {density}")
-
-    X = np.array([features['goal2'], features['duration2'],mnth,features['num_levels'],popularity,density,population]).reshape(1,-1)
-    prob = pipeline.predict_proba(X)[0, 1]
-    # prob = features['population']+features['density']
-
+    st=features['nature']
 # print("IS this shit still working????")
 #
 # def make_prediction(features):
@@ -86,8 +40,9 @@ def make_prediction(features):
 #     prob = 12
 
     result = {
-        'prediction': int(prob > 0.5),
-        'prob_succeed': round(100*prob,3)}
+        'prediction': int(0 > 0.5),
+        'prob_succeed': 0,
+        'nature':st}
     return result
 
 
